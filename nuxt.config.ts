018386efd48e0775054d3a57904ec70a5970a07b
Kem Guianalan <guianalankem@gmail.com>
2024-08-21 20:53:33 +0800

    Add VSCode settings and update Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,57 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import path from 'path';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  alias: 
+  {
+    "~": path.resolve(__dirname),
+  },
+  css: [
+    '~/assets/css/main.css',
+    'bootstrap/dist/css/bootstrap.min.css'
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: ['@nuxtjs/supabase'],
+  typescript: {
+    typeCheck: true
+  },
+  supabase: {
+      redirectOptions: {
+        login: '/',
+        include: [
+        ],
+        callback: '/confirm',
+        exclude: [
+          '/register',
+        ],
+        cookieRedirect: false,
+      },
+    cookieOptions: {
+      maxAge: 60 * 60 * 8,
+      sameSite: 'lax',
+      secure: true
+    }
+  },
+  plugins: [
+    '~/plugins/bootstrap.client.ts'
+  ],
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
+          type: 'text/javascript',
+        },
+        {
+          src: '/js/students-scripts.js'
+        }
+      ],
+    },
+  },
 })
---
+++ pages/calendar/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <h1>Weekly Calendar from July 22 to Sep 4, 2024</h1>
+      <WeeklyCalendar />
+    </div>
+  </template>
+
+  <script>
+  
+
+  import WeeklyCalendar from '~/components/WeeklyCalendar.vue';
+  
+  export default {
+    components: {
+      WeeklyCalendar,
+    },
+  };
+  
+  </script>
+  
