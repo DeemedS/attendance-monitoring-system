e3360ff576f32774820d812dd1bd759db13764cc
Kem Guianalan <guianalankem@gmail.com>
2024-08-28 16:35:19 +0800

    Refactor student list page to fetch subject and class data separately

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,125 +1,145 @@
 <template>
     <aside class="sidebar">
-        <ul>
-            <li><a :class="{ active: routeName === 'dashboard' }" href="/dashboard">Dashboard</a></li>
 
-            <div class="accordion">
-                <p class="text-center text-[18px] font-bold">Subjects</p>
+        <div class="menu-item" :class="{ active: activeMenu === 'dashboard' }"
+            @click="toggleMenu('dashboard'), navigateTo('dashboard')">
+            <i class="fas fa-tachometer-alt"></i> Dashboard
+        </div>
 
-                <!-- Subject 1 Accordion Item -->
-                <div class="accordion-item">
-                    <button class="accordion-header" @click="toggleAccordion('subject1')"
-                        :class="{ active: activeAccordion.subject === 'subject1' }">
-                        CMPE 40223
-                    </button>
-                    <div class="accordion-body" :class="{ 'is-active': activeAccordion.subject === 'subject1' }">
-                        <!-- Section 1 Accordion Item -->
-                        <div class="accordion-item ">
-                            <p class="text-center font-bold text-[16px]">Sections</p>
-                            <button class="accordion-header" @click="toggleAccordion('section1_1')"
-                                :class="{ active: activeAccordion.section === 'section1_1' }">
-                                2PCMPE-E4
-                            </button>
-                            <div class="accordion-body"
-                                :class="{ 'is-active': activeAccordion.section === 'section1_1' }">
-                                <ul class="function-list">
-                                    <li><a  :href="'/studentlist/' + subject_code + '/' + section_code">Student List</a></li>
-                                    <li><a  :href="'/classes/' + subject_code + '/' + section_code">Classes</a></li>
-                                    <li><a  href="#classCalendar1_1">Class Calendar</a></li>
-                                </ul>
-                            </div>
+        <div class="menu-item" :class="{ active: activeMenu === 'subjects' }" @click="toggleMenu('subjects')">
+            <i class="fas fa-book"></i> Subjects
+        </div>
+
+
+        <div v-if="activeMenu === 'subjects'" id="subjects" class="submenu">
+
+            <div v-for="(subject, index) in assignedSubjects" :key="index">
+                <div class="submenu-item" :class="{ active: activeSubmenu === subject.name }"
+                    @click="toggleSubmenu(subject.name)">
+                    {{ subject.name }}
+                </div>
+
+                <div v-if="activeSubmenu === subject.name" id="subject1" class="submenu">
+                    <div class="submenu-item">Sections</div>
+
+                    <div v-for="(section, index) in subject.sections" :key="index" class="submenu">
+
+                        <div class="submenu-item" :class="{ active: activeContent === section.name }"
+                            @click="toggleActiveContent(section.name)">
+                            {{ section.name }}
+                        </div>
+
+                        <div v-if="activeContent === section.name" class="section-content">
+                            <ul>
+                                <li class="content-item"
+                                    :class="{ active: activeContentItem === section.name + '-studentList' }"
+                                    @click="highlightContentItem(section.name + '-studentList')">
+                                    <NuxtLink :to="`/studentlist/${subject.name}/${section.name}`">Student List
+                                    </NuxtLink>
+                                </li>
+                                <li class="content-item"
+                                    :class="{ active: activeContentItem === section.name + '-classes' }"
+                                    @click="highlightContentItem(section.name + '-classes')">
+                                    <NuxtLink :to="`/classes/${subject.name}/${section.name}`">Classes</NuxtLink>
+                                </li>
+                                <li class="content-item"
+                                    :class="{ active: activeContentItem === section.name + '-calendar' }"
+                                    @click="highlightContentItem(section.name + '-calendar')">
+                                    <a href="#classcalendar">Class Calendar</a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
-            <li><a :class="{ active: routeName === 'logout' }" href="#">Logout</a></li>
-        </ul>
+        </div>
 
-
+        <div class="menu-item" @click="logout">
+            <i class="fas fa-sign-out-alt"></i> Logout
+        </div>
     </aside>
 </template>
 
 
 <script setup>
 import { ref } from 'vue';
-import { useRoute } from 'vue-router';
+
+const activeMenu = ref(null);
+const activeSubmenu = ref(null);
+const activeContent = ref(null);
+const activeContentItem = ref(null);
+const account_id = ref(null);
 
 const route = useRoute();
+const router = useRouter();
 const routeName = ref(route.name);
-const subject_code = "CMPE 40223";
-const section_code = "2PCMPE-E4";
 
-const activeAccordion = ref({
-    subject: '',
-    section: ''
+const assignedSubjects = ref([]);
+
+const { data: userid } = await useFetch('/api/me', {
+    headers: useRequestHeaders(['cookie'])
+})
+
+const { data: account, error: accountError } = await useFetch('/api/accounts', {
+    method: 'POST',
+    body: {
+        user_id: userid.value
+    }
 });
 
-function toggleAccordion(id) {
-    if (id.startsWith('subject')) {
-        activeAccordion.value = {
-            subject: activeAccordion.value.subject === id ? '' : id,
-            section: ''
-        };
+account_id.value = account.value.accountId
+
+const { data: subjects, error: subjectsError } = await useFetch('/api/subjects', {
+    method: 'POST',
+    body: {
+        assigned: account_id.value
+    }
+});
+
+assignedSubjects.value = subjects.value;
+
+
+if (routeName.value === 'dashboard') {
+    activeMenu.value = 'dashboard';
+} else if (routeName.value === 'studentlist-subject_code-section_code') {
+    activeMenu.value = 'subjects';
+    activeSubmenu.value = route.params.subject_code;
+    activeContent.value = route.params.section_code;
+    activeContentItem.value = route.params.section_code + '-studentList';
+} else if (routeName.value === 'classes-subject_code-section_code') {
+    activeMenu.value = 'subjects';
+    activeSubmenu.value = route.params.subject_code;
+    activeContent.value = route.params.section_code;
+    activeContentItem.value = route.params.section_code + '-classes';
+}
+
+function highlightContentItem(content) {
+    activeContentItem.value = content;
+}
+
+function toggleMenu(menu) {
+    if (menu === 'dashboard') {
+        activeMenu.value = 'dashboard';
     } else {
-        const subjectId = Object.keys(activeAccordion.value.subject).find(subject => activeAccordion.value.subject === subject);
-        activeAccordion.value = {
-            subject: activeAccordion.value.subject,
-            section: activeAccordion.value.section === id ? '' : id
-        };
+        activeMenu.value = menu;
     }
 }
+
+function toggleSubmenu(submenu) {
+    activeSubmenu.value = activeSubmenu.value === submenu ? null : submenu;
+}
+
+function toggleActiveContent(content) {
+    activeContent.value = activeContent.value === content ? null : content;
+}
+
+function navigateTo(route) {
+    router.push(`/${route}`);
+}
+
+function logout() {
+    document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    router.push('/');
+}
+
 </script>
-
-
-
-<style scoped>
-.accordion {
-    max-width: 600px;
-    margin: auto;
-}
-
-.accordion-item {
-    background-color: transparent;
-    border: none;
-    border: 0;
-    color: white;
-    margin-bottom: 10px;
-}
-
-.accordion-header {
-    border: none;
-    border: 0;
-    background-color: transparent;
-    text-align: center;
-    width: 100%;
-    font-size: 18px;
-}
-
-.accordion-body {
-    padding: 10px;
-    display: none;
-}
-
-.accordion-body.is-active {
-    display: block;
-}
-
-.accordion-body ul {
-    list-style-type: none;
-    padding: 0;
-    margin: 0;
-}
-
-.accordion-body ul li {
-    margin-bottom: 10px;
-}
-
-.accordion-body ul li a {
-    text-decoration: none;
-    color: white;
-}
-
-.accordion-header.active {
-    background-color: #ddd;
-}
-</style>
