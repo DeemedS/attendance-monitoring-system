7390ae1dedac312b259f62df5676aa1439ca2cf1
Kem Guianalan <guianalankem@gmail.com>
2024-08-29 00:01:47 +0800

    Add loading message and display additional class counts in class table

--- pages/classes/[subject_code]/[section_code].vue
+++ pages/classes/[subject_code]/[section_code].vue
@@ -15,19 +15,24 @@
 
                 <div class="totals flex flex-row gap-2">
                     <p>Total Classes: <span>{{ classCount }}</span></p>
+                    <p>No Classes: <span>{{ noClassCount }}</span></p>
+                    <p>Holidays: <span>{{ holidayCount }}</span></p>
                 </div>
 
-                <div class="table-container">
+                <div v-if="loadingContent" class="table-container">
+                    <p>Getting data, please wait...</p>
+                </div>
+
+                <div v-else class="table-container">
                     <table id="student-table">
                         <thead>
                             <tr>
                                 <th>INDEX</th>
                                 <th>DATE</th>
-                                <th>START TIME</th>
-                                <th>END TIME</th>
                                 <th>PRESENT</th>
                                 <th>ABSENT</th>
                                 <th>EXCUSED</th>
+                                <th>REMARKS</th>
                                 <th>EDIT</th>
                             </tr>
                         </thead>
@@ -35,11 +40,10 @@
                             <tr v-for="(classItem, index) in classArray" :key="classItem.id">
                                 <td>{{ index + 1 }}</td>
                                 <td>{{ convertDateToLocale(classItem.class_date) }}</td>
-                                <td>{{ classItem.start_time }}</td>
-                                <td>{{ classItem.end_time }}</td>
                                 <td>{{ classItem.presentCount }}</td>
                                 <td>{{ classItem.absentCount }}</td>
                                 <td>{{ classItem.excusedCount }}</td>
+                                <td>{{ classItem.remarks }}</td>
                                 <td><a :href="'/attendance/' + classItem.id">Edit</a></td>
                             </tr>
                         </tbody>
@@ -63,12 +67,17 @@ const { subject_code, section_code } = route.params;
 const classArray = ref([]);
 const classCount = ref(0);
 const subjectId = ref(null);
+const loadingContent = ref(true);
+const noClassCount = ref(0);
+const holidayCount = ref(0);
+
 
 const fetchSubject = async () => {
     try {
         const subject = await $fetch('/api/subjects', {
             method: 'POST',
             body: {
+                formAction: 'getSubjectId',
                 subject_code,
                 section_code,
             },
@@ -100,7 +109,14 @@ const fetchClasses = async () => {
 
         classArray.value.sort((a, b) => new Date(a.class_date) - new Date(b.class_date));
 
+        const noClasses = classArray.value.filter(item => item.remarks === 'No class').length;
+        const Holiday = classArray.value.filter(item => item.remarks === 'Holiday').length;
+
+
+        noClassCount.value = noClasses;
+        holidayCount.value = Holiday;
         classCount.value = classArray.value.length;
+
 
 
     } catch (error) {
@@ -114,7 +130,7 @@ const fetchAttendance = async (class_id) => {
 
     try {
 
-        const attendance= await $fetch('/api/attendance', {
+        const attendance = await $fetch('/api/attendance', {
             method: 'POST',
             body: { class_id: class_id },
         });
@@ -140,5 +156,6 @@ const fetchAttendance = async (class_id) => {
 onMounted(async () => {
     await fetchSubject();
     await fetchClasses();
+    loadingContent.value = false;
 });
 </script>
